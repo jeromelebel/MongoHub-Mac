b965f5990bd8b27efaa550e52f795997fc746a36
Anthony Williams <anthony.williams@listers.co.uk>
2014-06-05 16:40:41 +0100

    Use a fixed-width font for the insert data text view

--- Sources/ConnectionWindow/MHQueryWindowController.m
+++ Sources/ConnectionWindow/MHQueryWindowController.m
@@ -250,6 +250,9 @@
         }
     }
     
+    // Using a fixed-width font is a little easier on the eyes when dealing with JavaScript objects.
+    [self.insertDataTextView setFont:[NSFont fontWithName:@"Menlo" size:12]];
+    
     // Disable spell checking and substitutions.
     // When dealing with JavaScript objects, switching regular double quotes into smart quotes isn't helpful.
     [self.insertDataTextView setAutomaticDashSubstitutionEnabled:NO];
