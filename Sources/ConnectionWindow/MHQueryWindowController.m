4fd0b92edca9fc7f129e728121d5a587e158c9fa
Anthony Williams <anthony.williams@listers.co.uk>
2014-06-05 16:40:01 +0100

    Disable spell checking and substitutions
    OS X’s text substitutions aren’t helpful when dealing with code. Spell
    checking is rarely useful in these cases either.
    
    Conflicts:
    	Sources/ConnectionWindow/MHQueryWindowController.m

--- Sources/ConnectionWindow/MHQueryWindowController.m
+++ Sources/ConnectionWindow/MHQueryWindowController.m
@@ -249,6 +249,16 @@
             [fields addObject:field];
         }
     }
+    
+    // Disable spell checking and substitutions.
+    // When dealing with JavaScript objects, switching regular double quotes into smart quotes isn't helpful.
+    [self.insertDataTextView setAutomaticDashSubstitutionEnabled:NO];
+    [self.insertDataTextView setAutomaticDataDetectionEnabled:NO];
+    [self.insertDataTextView setAutomaticLinkDetectionEnabled:NO];
+    [self.insertDataTextView setAutomaticQuoteSubstitutionEnabled:NO];
+    [self.insertDataTextView setAutomaticSpellingCorrectionEnabled:NO];
+    [self.insertDataTextView setAutomaticTextReplacementEnabled:NO];
+    
     [findQueryLoaderIndicator start];
     [_mongoCollection findWithCriteria:criteria fields:fields skip:[_skipTextField intValue] limit:limit sort:sort callback:^(NSArray *documents, NSArray *bsonData, MODQuery *mongoQuery) {
         NSColor *currentColor;
