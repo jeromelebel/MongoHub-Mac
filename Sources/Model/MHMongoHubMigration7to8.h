ff05b5b15bb230eb452cd5fa5c5d6a2c7350b213
Jérôme Lebel <jerome@fotonauts.com>
2014-08-01 15:51:06 +0200

    wip for the next step to move passwords to keychain

---
+++ Sources/Model/MHMongoHubMigration7to8.h
@@ -0,0 +1,13 @@
+//
+//  MHMongoHubMigration7to8.h
+//  MongoHub
+//
+//  Created by Jérôme Lebel on 31/07/2014.
+//
+//
+
+#import <CoreData/CoreData.h>
+
+@interface MHMongoHubMigration7to8 : NSEntityMigrationPolicy
+
+@end
---
+++ Sources/Model/MHMongoHubMigration7to8.m
@@ -0,0 +1,45 @@
+//
+//  MHMongoHubMigration7to8.m
+//  MongoHub
+//
+//  Created by Jérôme Lebel on 31/07/2014.
+//
+//
+
+#import "MHMongoHubMigration7to8.h"
+#import "MHKeychain.h"
+
+@implementation MHMongoHubMigration7to8
+
+- (BOOL)createDestinationInstancesForSourceInstance:(NSManagedObject *)sourceInstance entityMapping:(NSEntityMapping *)mapping manager:(NSMigrationManager *)manager error:(NSError *__autoreleasing *)error
+{
+    NSManagedObjectContext *destMOC = manager.destinationContext;
+    NSManagedObject *newConnection;
+    NSString *password = [sourceInstance valueForKey:@"sshpassword"];
+    NSString *user = [sourceInstance valueForKey:@"sshuser"];
+    NSString *host;
+    NSString *hostAndPort;
+    
+    host = [sourceInstance valueForKey:@"sshhost"];
+    if ([[sourceInstance valueForKey:@"sshport"] integerValue] != 0) {
+        hostAndPort = [NSString stringWithFormat:@"%@:%@", [sourceInstance valueForKey:@"sshhost"], [sourceInstance valueForKey:@"sshport"]];
+    }
+    if (password && password.length > 0) {
+        BOOL result;
+        
+        result = [MHKeychain addOrUpdateInternetPasswordWithProtocol:kSecAttrProtocolSSH host:host port:[[sourceInstance valueForKey:@"sshport"] unsignedIntegerValue] account:user password:password];
+        if (!result) {
+            NSLog(@"can't save password for %@@%@", user, hostAndPort);
+        }
+    }
+    newConnection = [NSEntityDescription insertNewObjectForEntityForName:@"Connection" inManagedObjectContext:destMOC];
+    NSArray *keys = sourceInstance.entity.attributesByName.allKeys;
+    NSMutableDictionary *values = [[sourceInstance dictionaryWithValuesForKeys:keys] mutableCopy];
+    [values removeObjectForKey:@"sshpassword"];
+    [newConnection setValuesForKeysWithDictionary:values];
+    [manager associateSourceInstance:sourceInstance withDestinationInstance:newConnection forEntityMapping:mapping];
+    [values release];
+    return YES;
+}
+
+@end
