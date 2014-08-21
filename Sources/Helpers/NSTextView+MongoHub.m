5b4045a4d2c6e3dac6eeee5cec097f6d9b57037d
Jérôme Lebel <jerome@fotonauts.com>
2014-08-21 15:58:38 +0200

    don't deal the font here

--- Sources/Helpers/NSTextView+MongoHub.m
+++ Sources/Helpers/NSTextView+MongoHub.m
@@ -12,9 +12,6 @@
 
 - (void)mh_jsonSetup
 {
-    // Using a fixed-width font is a little easier on the eyes when dealing with JavaScript objects.
-    [self setFont:[NSFont fontWithName:@"Menlo" size:12]];
-
     // Disable spell checking and substitutions.
     // When dealing with JavaScript objects, switching regular double quotes into smart quotes isn't helpful.
     [self setAutomaticDashSubstitutionEnabled:NO];
