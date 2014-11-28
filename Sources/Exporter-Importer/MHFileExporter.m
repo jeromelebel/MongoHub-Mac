f7acafb28ebb7d0f0cb2b8a54c249fd09f9ffbfa
Jérôme Lebel <jerome@fotonauts.com>
2014-11-28 18:32:47 +0100

    make the default json key sort order as the document

--- Sources/Exporter-Importer/MHFileExporter.m
+++ Sources/Exporter-Importer/MHFileExporter.m
@@ -24,6 +24,7 @@
     if (self = [self init]) {
         _collection = [collection retain];
         _exportPath = [exportPath retain];
+        self.jsonKeySortOrder = MODJsonKeySortOrderDocument;
     }
     return self;
 }
