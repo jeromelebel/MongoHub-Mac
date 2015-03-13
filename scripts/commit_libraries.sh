da773fed625d50197e7b1fdb6e89276d2c331875
Jérôme Lebel <jerome@fotonauts.com>
2015-03-13 01:13:14 +0100

    adding a tool to update sha1 for libraries (libbson and mongo-c-driver)

--- scripts/commit_libraries.sh
+++ scripts/commit_libraries.sh
@@ -3,14 +3,14 @@
 set -eux
 
 COMMENT="$1"
-if [ "$COMMENT" = "" ] ; then
+if [ "${COMMENT}" = "" ] ; then
   echo "Usage: $(basename $0) COMMENT"
   exit 1
 fi
 
 cd Libraries/MongoObjCDriver
-./scripts/commit_libraries.sh "${VERSION}"
+./scripts/commit_libraries.sh "${COMMENT}"
 cd ../..
 
 git submodule status | sed 's/^.//' | awk '{ print $1 }' > Libraries/MongoObjCDriver.sha1
-git commit -m "${VERSION}" .
+git commit -m "${COMMENT}" .
